077fd5c17c739a35634aa6078cb686211ae6fc02
TheIthorian <ben.shortland1020@gmail.com>
2023-12-30 16:36:45 +0000

    feat: add multithreading

--- go-processor/main.go
+++ go-processor/main.go
@@ -77,8 +77,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 func createGIF(files []*multipart.FileHeader, fps int, size int) (string, error) {
 	log.Println("creating gif from " + fmt.Sprint(len(files)) + " files")
 
-	var images []*image.Paletted
-	var delays []int
+	var images = make([]*image.Paletted, len(files))
 
 	tempDir, err := os.MkdirTemp("", "uploaded-images")
 	if err != nil {
@@ -87,66 +86,17 @@ func createGIF(files []*multipart.FileHeader, fps int, size int) (string, error)
 	}
 	defer os.RemoveAll(tempDir)
 
+	ch := make(chan *image.Paletted, len(files))
 	for i, file := range files {
-		log.Println("processing file " + fmt.Sprint(i))
+		fmt.Println("Processing file: " + fmt.Sprint(i))
+		go processImageAndAddToArray(file, fps, size, tempDir, ch)
+	}
 
-		src, err := file.Open()
-		if err != nil {
-			log.Println(err)
-			return "", err
-		}
-		defer src.Close()
-
-		// Save the file to the temporary directory
-		tempFile, err := os.CreateTemp(tempDir, "uploaded-image-*.jpg")
-		if err != nil {
-			log.Println(err)
-			return "", err
-		}
-		defer tempFile.Close()
-
-		_, err = io.Copy(tempFile, src)
-		if err != nil {
-			log.Println(err)
-			return "", err
-		}
-
-		// Rewind the temporary file before processing
-		tempFile.Seek(0, io.SeekStart)
-
-		var img image.Image
-		_, format, err := image.DecodeConfig(tempFile)
-		if err != nil {
-			return "", err
-		}
-
-		// Rewind the temporary file before decoding
-		tempFile.Seek(0, io.SeekStart)
-
-		switch format {
-		case "jpeg":
-			img, err = jpeg.Decode(tempFile)
-			if err != nil {
-				return "", err
-			}
-		case "png":
-			img, err = png.Decode(tempFile)
-			if err != nil {
-				return "", err
-			}
-		default:
-			return "", fmt.Errorf("unsupported image format: %s", format)
-		}
-
-		// Resize, crop, and process the image as before
-		resizedImg := resize.Resize(uint(size), 0, img, resize.Lanczos3)
-		croppedImg := resize.Resize(uint(size), uint(size), resizedImg, resize.Lanczos3)
-
-		palettedImg := image.NewPaletted(croppedImg.Bounds(), palette)
-		draw.FloydSteinberg.Draw(palettedImg, croppedImg.Bounds(), croppedImg, image.Point{})
-
-		images = append(images, palettedImg)
-		delays = append(delays, fps)
+	fmt.Println("Waiting for channels")
+	for i := range files {
+		fmt.Println("Reading from channel: " + fmt.Sprint(i))
+		image := <-ch
+		images[i] = image
 	}
 
 	gifPath := "output.gif"
@@ -156,6 +106,11 @@ func createGIF(files []*multipart.FileHeader, fps int, size int) (string, error)
 		return "", err
 	}
 	defer gifFile.Close()
+
+	var delays = make([]int, len(files))
+	for i := range delays {
+		delays[i] = fps
+	}
 
 	err = gif.EncodeAll(gifFile, &gif.GIF{
 		Image: images,
@@ -168,6 +123,72 @@ func createGIF(files []*multipart.FileHeader, fps int, size int) (string, error)
 
 	log.Println("\tencoded gif")
 	return gifPath, nil
+}
+
+func processImageAndAddToArray(
+	file *multipart.FileHeader,
+	fps int,
+	size int,
+	tempDir string,
+	ch chan *image.Paletted,
+) {
+	src, err := file.Open()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer src.Close()
+
+	// Save the file to the temporary directory
+	tempFile, err := os.CreateTemp(tempDir, "uploaded-image-*.jpg")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, src)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Rewind the temporary file before processing
+	tempFile.Seek(0, io.SeekStart)
+
+	var img image.Image
+	_, format, err := image.DecodeConfig(tempFile)
+	if err != nil {
+		return
+	}
+
+	// Rewind the temporary file before decoding
+	tempFile.Seek(0, io.SeekStart)
+
+	switch format {
+	case "jpeg":
+		img, err = jpeg.Decode(tempFile)
+		if err != nil {
+			return
+		}
+	case "png":
+		img, err = png.Decode(tempFile)
+		if err != nil {
+			return
+		}
+	default:
+		return
+	}
+
+	// Resize, crop, and process the image as before
+	resizedImg := resize.Resize(uint(size), 0, img, resize.Bicubic)
+	croppedImg := resize.Resize(uint(size), uint(size), resizedImg, resize.Bicubic)
+
+	palettedImg := image.NewPaletted(croppedImg.Bounds(), palette)
+	draw.FloydSteinberg.Draw(palettedImg, croppedImg.Bounds(), croppedImg, image.Point{})
+
+	// images = append(images, palettedImg)
+	ch <- palettedImg
 }
 
 func resultHandler(w http.ResponseWriter, r *http.Request) {
